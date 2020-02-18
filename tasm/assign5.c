4f671d57f3dbb68961877b199e115b888431e161
Muskaan Shraogi <54302920+muskaanshraogi@users.noreply.github.com>
2020-02-18 14:39:55 +0530

    Add files via upload

--- tasm/assign5.c
+++ tasm/assign5.c
@@ -4,7 +4,8 @@
 #include<conio.h>
 
 void createDirectory();
-void deleteDirectory();
+void deleteFile();
+void copyFile();
 union REGS inreg, outreg;	//i/o registers
 struct SREGS segreg;		//segment register
 
@@ -12,7 +13,6 @@ struct SREGS segreg;		//segment register
 void main()
 {
 	int choice;
-	
 	clrscr();
 	
 	do
@@ -26,7 +26,10 @@ void main()
 			createDirectory();
 			break;
 		case 2:
-			deleteDirectory();
+			deleteFile();
+			break;
+		case 3:
+			copyFile();
 			break;
 		case 4:
 			exit(0);
@@ -36,10 +39,10 @@ void main()
 
 void createDirectory()
 {
-	char dirname[20];
+	char dirname[20];	
 	
-	printf("\nEnter a name for the new directory :\n");
-	scanf("%s",dirname);
+	printf("\nEnter a name for the new sub-directory :\n");
+	scanf("%s",dirname);	
 	
 	inreg.h.ah = 0x39; //create sub-directory
 	inreg.x.dx = FP_OFF(dirname);
@@ -52,19 +55,88 @@ void createDirectory()
 		printf("\nDirectory created successfully.\n");
 }
 
-void deleteDirectory()
+void deleteFile()
 {
 	char filename[20];
 	
 	printf("\nEnter the name of the file to be deleted :\n");
-	scanf("%s",filename)
+	scanf("%s",filename);
 	
 	inreg.h.ah = 0x41;
 	inreg.x.dx = FP_OFF(filename);
-	
 	int86(0x21, &inreg, &outreg);
+	
 	if(outreg.x.cflag)
 		printf("\nError! File could not be deleted.\n");
 	else
 		printf("\nFile deleted successfully.\n");
+}
+
+void copyFile()
+{
+	char filename[20], newfile[20];
+	int h1, h2;
+	
+	printf("\nEnter the name of the file to be copied :\n");
+	scanf("%s",filename);
+	
+	inreg.h.ah = 0x3D;	//open file in read mode
+	inreg.x.dx = FP_OFF(filename);
+	inreg.h.al = 0x00;
+	int86(0x21, &inreg, &outreg);
+	h1 = outreg.x.ax;
+	
+	if(outreg.x.cflag)
+		printf("\nError! File %s could not be opened.\n",filename);
+	else
+	{
+		printf("\nEnter a name for the new file :\n");
+		scanf("%s",newfile);
+		
+		//inreg.h.ah = 0x3C;	//create file
+		//inreg.x.dx = FP_OFF(filename);
+		//inreg.x.cx = 0x00;
+		//int86(0x21, &inreg, &outreg);
+		
+		//if(!outreg.x.cflag)
+		//{
+			inreg.h.ah = 0x3D;	//open file in write mode
+			inreg.x.dx = FP_OFF(newfile);
+			inreg.h.al = 0x01;
+			int86(0x21, &inreg, &outreg);
+			h2 = outreg.x.ax;
+			
+			inreg.h.ah = 0x3F;	//read file
+			inreg.x.bx = h1;
+			inreg.x.cx = 0xFF;
+			inreg.x.dx = FP_OFF(filename);
+			segreg.ds = FP_SEG(filename);
+			int86x(0x21, &inreg, &outreg, &segreg);
+			
+			if(!outreg.x.cflag)
+			{
+				printf("\nReading file....\n");
+				
+				inreg.h.ah = 0x40;	//write to file
+				inreg.x.bx = h2;
+				inreg.x.cx = 0xFF;
+				inreg.x.dx = FP_OFF(newfile);
+				segreg.ds = FP_SEG(newfile);
+				int86x(0x21, &inreg, &outreg, &segreg);
+				
+				if(outreg.x.cflag)
+					printf("\nError copying file!\n");
+				else
+					printf("\nFile copied successfully!\n");
+			}
+		//}
+	}
+	
+	inreg.h.ah = 0x3E;	//close file
+	inreg.x.dx = FP_OFF(newfile);
+	int86(0x21, &inreg, &outreg);
+	
+	inreg.h.ah = 0x3E;	//close file
+	inreg.x.dx = FP_OFF(filename);
+	int86(0x21, &inreg, &outreg);
 }
